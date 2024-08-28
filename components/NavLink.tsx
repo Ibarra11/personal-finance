ba93b10ea9052c69c4260a25c928cd22454af1aa
Alan Ibarra <alanjibarradev@gmail.com>
2024-08-28 15:11:25 -0700

    fix overflow issue and remove unnecessary imports

--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { FC, ReactNode, SVGProps } from "react";
+import { ReactNode } from "react";
 
 interface Props {
   href: string;
