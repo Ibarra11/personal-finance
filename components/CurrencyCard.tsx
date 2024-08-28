ffbe022b1775620ab6e89d8bf315c36c3821b6ef
Alan Ibarra <alanjibarradev@gmail.com>
2024-08-28 14:09:57 -0700

    fix padding for currency cards

--- components/CurrencyCard.tsx
+++ components/CurrencyCard.tsx
@@ -10,14 +10,16 @@ export default function CurrencyCard({
   isFeatured?: boolean;
 }) {
   return (
-    <Card className={`space-y-3 ${isFeatured ? "bg-gray-900" : "bg-white"}`}>
+    <Card
+      className={`space-y-3 p-5 md:p-6 ${isFeatured ? "bg-gray-900" : "bg-white"}`}
+    >
       <CardHeader
-        className={`pb-0 text-sm ${isFeatured ? "text-white" : "text-gray-500"}`}
+        className={`p-0 text-sm ${isFeatured ? "text-white" : "text-gray-500"}`}
       >
         {title}
       </CardHeader>
       <CardContent
-        className={`text-3xl font-bold ${isFeatured ? "text-white" : "text-gray-900"}`}
+        className={`p-0 text-3xl font-bold ${isFeatured ? "text-white" : "text-gray-900"}`}
       >
         ${amount}
       </CardContent>
