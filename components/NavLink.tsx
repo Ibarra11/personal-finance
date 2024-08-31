f331784c5001b867455d9f934ede56194e5442a9
Alan Ibarra <alanjibarradev@gmail.com>
2024-08-30 21:10:55 -0700

    Fix issues with navlink

--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -15,10 +15,9 @@ export default function NavLink({
 }: React.PropsWithChildren<Props>) {
   const pathname = usePathname();
   const active = pathname === href;
-
   return (
     <Link
-      className={`flex min-h-11 items-center justify-center rounded-tl-lg rounded-tr-lg text-xs font-bold md:flex-col md:justify-start md:gap-1 md:pb-3 md:pt-2 lg:flex-row lg:gap-4 lg:rounded-br-xl lg:rounded-tr-xl lg:border-b-0 lg:px-8 lg:py-4 lg:text-base ${active ? "border-green border-b-4 bg-background text-gray-900" : ""} `}
+      className={`flex min-h-11 items-center justify-center rounded-tl-lg rounded-tr-lg text-xs font-bold md:flex-col md:justify-start md:gap-1 md:pb-3 md:pt-2 lg:flex-row lg:gap-4 lg:rounded-br-xl lg:rounded-tl-none lg:rounded-tr-xl lg:border-b-0 lg:px-8 lg:py-4 lg:text-base ${active ? "border-green border-b-4 bg-background text-gray-900 lg:border-l-4" : ""} `}
       href={href}
     >
       <span className={active ? "text-green" : ""}>{icon}</span>
