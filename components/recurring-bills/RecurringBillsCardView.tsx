f8305238487a382a21db6338bb7c9114c74b9c3c
Alan Ibarra <alanjibarradev@gmail.com>
2024-09-27 16:44:38 -0700

    rearrange layout for recurring bills card

--- components/recurring-bills/RecurringBillsCardView.tsx
+++ components/recurring-bills/RecurringBillsCardView.tsx
@@ -18,19 +18,17 @@ export function RecurringBillsCardView({ recurringBills }: Props) {
 
 function RecurringBillCard({ name, dueDate, amount }: RecurringBill) {
   return (
-    <div className="space-y-2 border-b border-gray-100 py-5 first:pt-0 last:border-b-0 last:pb-0">
-      <h5 className="text-sm font-bold text-gray-900">{name}</h5>
-      <div className="flex justify-between">
+    <div className="flex items-center justify-between space-y-2 border-b border-gray-100 py-5 first:pt-0 last:border-b-0 last:pb-0">
+      <div className="flex flex-col gap-2">
+        <h5 className="text-sm font-bold text-gray-900">{name}</h5>
         <div className="flex gap-2">
           <p className="text-xs text-green">
             Monthly - {getDayWithSuffix(dueDate)}
           </p>
           <IconBillPaid className="size-4 text-green" />
         </div>
-        <p className="text-sm font-bold text-gray-900">
-          ${formatNumber(amount)}
-        </p>
       </div>
+      <p className="text-sm font-bold text-gray-900">${formatNumber(amount)}</p>
     </div>
   );
 }
